9651700f9748c8f6e26fe9dd0a5e3fd10a75f41c
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2021-03-21 17:50:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+/* function Car(wheel, smth, engine, name) {
+
+    this.wheel = wheel;
+    this.color = smth;
+    this.engine = engine;
+    this.name = name;
+    
+    this.run = () => {
+      console.log("we run")
+      console.log(this)
+    } 
+    
+    }
+    
+    
+    const thing = new Car(4, "blue", "v12", "test")
+    thing.run() */
+
+    /* class Car {
+        countWheels = 4
+        color = ''
+        engine = ''
+        name = ''
+      
+        constructor(countWheels, color, engine, name){
+          this.countWheels = countWheels
+          this.color = color
+          this.engine = engine
+          this.name = name
+        }
+
+        run() {
+            console.log("we run" + this.name)
+        }
+
+        givename() {
+            console.log(this.name)
+        }
+
+        log() {
+            console.log(this)
+        }
+
+      }
+
+      
+    const lol = new Car(4, "red", "v12", "smth")  
+    lol.log() */
+
+    //if u see this text, know i'm working about code here and ik i copied html and css, it's boring to do html and css, but i will do my js
+    
